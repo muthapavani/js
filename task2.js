dc945f01d9d2abe2f671c66e748d08ee999fa819
muthapavani <muttapavani9@gmail.com>
2024-09-19 19:16:33 +0530

    push

---
+++ task2.js
@@ -0,0 +1,98 @@
+
+var num=6
+if(num==0){
+    console.log("zero")
+}
+else if(num>0){
+    console.log("positive")
+    if(num%2==0){
+        console.log("positive,even")
+    }
+    else{
+        console.log("negative,odd")
+    }
+}
+else{
+    console.log("negative")
+   }
+
+
+
+   var num=0
+   if(num<0){
+    console.log("negative")
+   }
+   else if(num>0){
+    console.log("positive")
+     if (num%2==0){
+        comnsole.log("even")
+     }
+     else{
+        console.log("odd")
+     }
+   }
+   else{
+    console.log("zero")
+   }
+
+
+   var a=7
+   if(a<0){
+    console.log("negative")
+   }
+   else if(a>0){
+    console.log("positive")
+    if(a%2==0){
+       console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+   }
+   else{
+    console.log("zero")
+   }
+
+   var a=-1
+   if(a==0){
+    console.log("zero")
+   }
+   else if(a>0){
+    console.log("positive")
+    if(a%2==0){
+       console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+   }
+   else{
+    console.log("negative")
+   }
+
+
+
+   var n1=4
+   var n2=6
+   if(n1%2==0 && n2%2==0){
+     console.log(n1+n2)
+   }
+   else{
+    console.log("not valid")
+   }
+
+   var n1=5
+   var n2=4
+   if(n1%2==0 && n2%2==0){
+     console.log(n1+n2)
+   }
+   else{
+    console.log("not valid")
+   }
+
+
+   n=5
+   for (var i=1; i<=10; i++){
+      console.log(n+ "*" +i+ "=" +n*i )
+   }
+
